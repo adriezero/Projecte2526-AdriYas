42e1e2aa7b93720eaa31bb86c1b5b6bc1c74e2d4
adriezero <adrimoodle33@gmail.com>
2026-02-27 16:01:36 +0100

    Proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
-export function Proxy(request: NextRequest) {
+export function proxy(request: NextRequest) {
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/home', request.url))
   }
