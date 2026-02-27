6649814ddf8372239211200d3dcf5b06be649a14
adriezero <adrimoodle33@gmail.com>
2026-02-27 16:04:39 +0100

    Proxy

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,14 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
-export function proxy(request: NextRequest) {
+export default function proxy(request: NextRequest) {
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/home', request.url))
   }
   
   return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
 }
