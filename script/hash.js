970ee284f79052cc44e00f5d52fd1feab6067955
adriezero <adrimoodle33@gmail.com>
2026-03-11 18:46:56 +0100

    Script para tener contraseñas hasheadas

--- script/hash.js
+++ script/hash.js
@@ -1,15 +1,15 @@
 const bcrypt = require('bcrypt');
-const { hash } = require('node:crypto');
-let password="carla";
+
+let password = "carla";
 
 bcrypt.hash(password,10).then(hash =>{
-    console.log(hash)
-
+    console.log(hash);
 })
 
-/* -- Failed query:
--- UPDATE dispatcher SET Contraseña = '$2b$10$ebr3/GwJaqO5APEGu1F9YuuriB32v8Xv4soKLUeFWQ6jXQa8Jd5rK' WHERE dispatcher.Email = 'juan@truckwave.com'
-UPDATE dispatcher SET "Contraseña" = '$2b$10$rOoIHg9swvDJsbbnv5Wg4.oBzdqfAaOAiC3HhsjStBAJj40/c1KbS' WHERE dispatcher."Email" = 'carla@truckwave.com' */
-/* 
-cfgs@insmarianao:~/Escriptori/Proyecto/truckwave$ node script/hash.js
-$2b$10$80SdfHbxopAXgQR.8KJZiOj8m5bGS0gDZutRiE60qnsOhsP5eaDre */
+// Para ejecutarlo: "node scripts/hash.js", ejemplo:
+/* cfgs@insmarianao:~/Escriptori/Proyecto/truckwave$ node script/hash.js
+    $2b$10$80SdfHbxopAXgQR.8KJZiOj8m5bGS0gDZutRiE60qnsOhsP5eaDre */
+
+// Luego se actualiza a la BBDD de Neon
+
+/*UPDATE dispatcher SET "Contraseña" = 'contraseña hasheada' WHERE dispatcher."Email" = 'carla@truckwave.com' */
